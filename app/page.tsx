82c1cd48401e619fe98bc57af89559ae0fca2676
kiraaziz <kiraaziz2020@gmail.com>
2023-10-28 11:29:34 +0200

    last

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,34 @@
-import Navbar from '@/components/Navbar'
+import Swiper from '@/components/Swiper'
+import Navbar from '@/components/Navbar2'
 import S1 from '@/components/S1'
 import S2 from '@/components/S2'
 import S3 from '@/components/S3'
 import S4 from '@/components/S4'
+import Box from '@/components/Box'
 
-const Page = () => {
+
+
+const Page = async() => {
+
+    await new Promise((res)=>setTimeout(res, 2500))
 
     return (
         <>
             <Navbar />
-            <S1 />
-            <S2 />
-            <S3 />
-            <S4 />
+            <Swiper />
+            <Box>
+                <S1 />
+            </Box>
+            <Box>
+                <S2 />
+            </Box>
+            <Box>
+                <S3 />
+            </Box>
+            <Box>
+                <S4 />
+            </Box>
+
         </>
     )
 }
